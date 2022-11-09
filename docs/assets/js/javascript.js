bda08bf27f1883163f86ccd5d653b4b9e9c57413
ichsanbudimanp <114840300+ichsanbudimanp@users.noreply.github.com>
2022-11-09 21:58:02 +0700

    Add files via upload

---
+++ docs/assets/js/javascript.js
@@ -0,0 +1,7 @@
+document.getElementById("myBtn").onclick = function() {myFunction()};
+
+function myFunction() {
+  document.getElementById("navbar2").classList.toggle("show");
+  document.getElementById("order").classList.toggle("pasif");
+}
+
