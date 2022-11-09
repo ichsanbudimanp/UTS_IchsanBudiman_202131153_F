9881dfaca539f56a643226a5e2234ec4ac29cef0
ichsanbudimanp <114840300+ichsanbudimanp@users.noreply.github.com>
2022-11-09 22:13:16 +0700

    Delete docs directory

--- docs/assets/js/javascript.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("myBtn").onclick = function() {myFunction()};
-
-function myFunction() {
-  document.getElementById("navbar2").classList.toggle("show");
-  document.getElementById("order").classList.toggle("pasif");
-}
-
